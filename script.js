2b6901d3b399e3a58cc054a7af715a7a532ecaff
LSL-Coding51 <168805232+LSL-Coding51@users.noreply.github.com>
2024-06-27 19:41:34 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-
+const searchForm = document.getElementBYId("search-form");
+const searchBox = document.getElementBYId("search-box");
+const searchResult = document.getElementBYId("search-result");
+const showMoreBtn = document.getElementBYId("show-more-btn");
