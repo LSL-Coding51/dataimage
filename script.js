93879d66a534c08b3151916ac6074a7a745fe0f7
LSL-Coding51 <168805232+LSL-Coding51@users.noreply.github.com>
2024-06-27 19:56:56 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
+const ACCESS_KEY = "H9E7Cm1F7xUK1tCq2s3UPSFPAXPBA2qQJ6SobC3Svvc"
 const searchForm = document.getElementBYId("search-form");
 const searchBox = document.getElementBYId("search-box");
 const searchResult = document.getElementBYId("search-result");
 const showMoreBtn = document.getElementBYId("show-more-btn");
+let keyword = "";
+let page = 1;
+async function searchImages(){
+  keyword = searchbox.value;
+  const url = "https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${ACCESS_KEY}";
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data)
+}
+searchForm.addEventListener("submit", (e) =>{
+  e.preventDefault();
+  page = 1;
+})
